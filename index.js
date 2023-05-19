f87fb5398da2c4ca374fbee5fcdd0fb0b94ce183
Oli Ulllah <62113494+OliUllah1@users.noreply.github.com>
2023-05-19 08:02:47 +0600

    set cors and middleware

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
   res.send('toys server is runing')
