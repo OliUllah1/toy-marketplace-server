5a062ed85f032b624e7cc29399aff248b90ac7a9
Oli Ulllah <62113494+OliUllah1@users.noreply.github.com>
2023-05-20 11:54:30 +0600

    find the single data on the  add my toys

--- index.js
+++ index.js
@@ -62,6 +62,12 @@ async function run() {
       const result = await toysCollection.find(query).toArray()
       res.send(result)
     })
+    app.get('/mytoys/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await toysCollection.findOne(query);
+      res.send(result)
+    })
     app.delete('/mytoys/:id',async(req,res)=>{
       const id = req.params.id;
       const query ={_id:new ObjectId(id)}
